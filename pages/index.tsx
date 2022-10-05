3472dc95a89c307163c794c647c9aa7db0248e45
veejess <87065795+veejess@users.noreply.github.com>
2022-10-05 19:34:50 +1300

    Customising

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,12 @@ const Home: NextPage = () => {
   return (
       <div className="app">
         <Head>
-          <title>My Links</title>
+          <title>Jessica Villegas</title>
           <meta name="description" content={`${name}'s Links`}/>
           <link rel="icon" href="/favicon.ico"/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet"/>
         </Head>
 
         <main>
